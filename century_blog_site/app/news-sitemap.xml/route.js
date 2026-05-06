99b130d68ec7fc0141ca21ab2d1133ddff34d1fb
theovin4 <theovincenzo@gmail.com>
2026-05-06 01:29:48 +0100

    Stabilize news sitemap output

--- century_blog_site/app/news-sitemap.xml/route.js
+++ century_blog_site/app/news-sitemap.xml/route.js
@@ -16,14 +16,21 @@ export const dynamic = "force-dynamic";
 export async function GET() {
   const siteUrl = getSiteUrl();
   const cutoff = Date.now() - (2 * 24 * 60 * 60 * 1000);
-  const posts = (await getPosts())
-    .filter((post) => post?.slug)
-    .filter((post) => new Date(post.sitePublishedAt || post.publishedAt || post.updatedAt).getTime() >= cutoff)
-    .slice(0, 1000);
+  const posts = (await getPosts()).filter((post) => post?.slug).slice(0, 1000);
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">\n${posts
-    .map((post) => `  <url>\n    <loc>${escapeXml(`${siteUrl}/news/${post.slug}`)}</loc>\n    <news:news>\n      <news:publication>\n        <news:name>${escapeXml("Century Blog")}</news:name>\n        <news:language>en</news:language>\n      </news:publication>\n      <news:publication_date>${escapeXml(new Date(post.sitePublishedAt || post.publishedAt || post.updatedAt).toISOString())}</news:publication_date>\n      <news:title>${escapeXml(post.title)}</news:title>\n    </news:news>\n  </url>`)
-    .join("\n")}\n</urlset>`;
+  const xmlEntries = posts
+    .map((post) => {
+      const publicationDate = new Date(post.sitePublishedAt || post.publishedAt || post.updatedAt);
+      const hasRecentNewsMetadata = publicationDate.getTime() >= cutoff;
+      const newsBlock = hasRecentNewsMetadata
+        ? `\n    <news:news>\n      <news:publication>\n        <news:name>${escapeXml("Century Blog")}</news:name>\n        <news:language>en</news:language>\n      </news:publication>\n      <news:publication_date>${escapeXml(publicationDate.toISOString())}</news:publication_date>\n      <news:title>${escapeXml(post.title)}</news:title>\n    </news:news>`
+        : "";
+
+      return `  <url>\n    <loc>${escapeXml(`${siteUrl}/news/${post.slug}`)}</loc>${newsBlock}\n  </url>`;
+    })
+    .join("\n");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">\n${xmlEntries}\n</urlset>`;
 
   return new NextResponse(xml, {
     headers: {
