140da1830f85d2b14a62f195edfca88e86441c75
theovin4 <theovincenzo@gmail.com>
2026-05-06 14:48:02 +0100

    Simplify news sitemap XML output

--- century_blog_site/app/news-sitemap.xml/route.js
+++ century_blog_site/app/news-sitemap.xml/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getPosts } from "@/lib/posts-store";
-import { getSiteUrl } from "@/lib/site";
+import { getSiteUrl, normalizeStoredText } from "@/lib/site";
 
 function escapeXml(value) {
   return String(value || "")
@@ -13,6 +13,13 @@ function escapeXml(value) {
 
 export const dynamic = "force-dynamic";
 
+function renderNewsUrl(siteUrl, post) {
+  const publicationDate = new Date(post.sitePublishedAt || post.publishedAt || post.updatedAt).toISOString();
+  const title = normalizeStoredText(post.title);
+
+  return `<url><loc>${escapeXml(`${siteUrl}/news/${post.slug}`)}</loc><news:news><news:publication><news:name>Century Blog</news:name><news:language>en</news:language></news:publication><news:publication_date>${escapeXml(publicationDate)}</news:publication_date><news:title>${escapeXml(title)}</news:title></news:news></url>`;
+}
+
 export async function GET() {
   const siteUrl = getSiteUrl();
   const cutoff = Date.now() - (2 * 24 * 60 * 60 * 1000);
@@ -21,15 +28,8 @@ export async function GET() {
     .filter((post) => new Date(post.sitePublishedAt || post.publishedAt || post.updatedAt).getTime() >= cutoff)
     .slice(0, 1000);
 
-  const xmlEntries = posts
-    .map((post) => {
-      const publicationDate = new Date(post.sitePublishedAt || post.publishedAt || post.updatedAt);
-
-      return `  <url>\n    <loc>${escapeXml(`${siteUrl}/news/${post.slug}`)}</loc>\n    <news:news>\n      <news:publication>\n        <news:name>${escapeXml("Century Blog")}</news:name>\n        <news:language>en</news:language>\n      </news:publication>\n      <news:publication_date>${escapeXml(publicationDate.toISOString())}</news:publication_date>\n      <news:title>${escapeXml(post.title)}</news:title>\n    </news:news>\n  </url>`;
-    })
-    .join("\n");
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">\n${xmlEntries}\n</urlset>`;
+  const xmlEntries = posts.map((post) => renderNewsUrl(siteUrl, post)).join("");
+  const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">${xmlEntries}</urlset>`;
 
   return new NextResponse(xml, {
     headers: {
